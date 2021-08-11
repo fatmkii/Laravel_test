d5caecf3aba5661e8174d76b5f84f9b2ebfaf24f
fatmkii <47155837@qq.com>
2021-08-11 21:33:37 +0800

    尝试解决ios上跳问题

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    {{-- <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> --}}
     <title> @yield('title',config('app.name'))</title>
 </head>
 
